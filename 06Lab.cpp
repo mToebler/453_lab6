ddf235311fe3e6fd00aafc147889b063dbd34b52
mayegirl <59669195+mayegirl@users.noreply.github.com>
2020-10-23 21:57:23 -0600

    Update 06Lab.cpp

--- 06Lab.cpp
+++ 06Lab.cpp
@@ -312,7 +312,13 @@ void AdditionalStatementAttackTests() {
 
 
 void CommentAttackTests() {
-    std::cout << "****COMMENT - UNDER CONSTRUCTION****" << std::endl << std::endl;
+    std::cout << "****COMMENT ATTACK****" << std::endl << std::endl;
+
+    //Test #1. Inserts a comment to the end of the user so the program ignores the password prompt.
+    std::string username = "user'; -- ";
+    std::string password = "password";
+
+    RunTest(username, password);
 }
 
 
